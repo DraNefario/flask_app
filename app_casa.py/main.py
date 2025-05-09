6ef4866f1337f603e6d629b0d1f1add8fd9b8ab9
DraNefario <162912616+DraNefario@users.noreply.github.com>
2025-05-09 18:26:54 -0300

    Update main.py

--- app_casa.py/main.py
+++ app_casa.py/main.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, request
+from flask import Flask
 from actuators import actuators
 from login import user
 from sensors import sensors
+from mqtt_routes import mqtt_routes, init_mqtt
 
-# Inicializa o aplicativo Flask
+from flask_mqtt import Mqtt
+import flask_login
+
 app = Flask(__name__)
-
 app.secret_key = 'newton'
 
+# MQTT Config
+app.config['MQTT_BROKER_URL'] = 'broker.emqx.io'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_USERNAME'] = ''
+app.config['MQTT_PASSWORD'] = ''
+app.config['MQTT_KEEPALIVE'] = 60
+app.config['MQTT_TLS_ENABLED'] = False
+
+mqtt = Mqtt(app)
+
+# Register blueprints
 app.register_blueprint(actuators)
 app.register_blueprint(user)
 app.register_blueprint(sensors)
+app.register_blueprint(mqtt_routes)
 
-# Iniciar o app
+# Inicia MQTT listeners
+init_mqtt(app, mqtt)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
