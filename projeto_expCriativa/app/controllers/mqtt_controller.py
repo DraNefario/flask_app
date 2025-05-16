32aab175ac9f5a4a0f36409c30dd63fb4e0c7de6
DraNefario <162912616+DraNefario@users.noreply.github.com>
2025-05-15 22:34:25 -0300

    Delete projeto_expCriativa/app directory

--- projeto_expCriativa/app/controllers/mqtt_controller.py
+++
@@ -1,9 +0,0 @@
-# app/controllers/mqtt_controller.py
-
-from app.mqtt.cliente import get_mqtt_data, publish_message
-
-def handle_get_mqtt_data():
-    return get_mqtt_data()
-
-def handle_publish_message(data):
-    return publish_message(data['topic'], data['message'])
