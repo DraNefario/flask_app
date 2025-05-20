e96b3d88e1cd0c4d5d087cf006cf4f4ff75d6d94
Thomas Manussadjian Steinhausser <162912616+DraNefario@users.noreply.github.com>
2025-05-20 08:16:25 -0300

    Update app.py

--- fazenda/app.py
+++ fazenda/app.py
@@ -5,4 +5,4 @@ from models.__init__ import create_app
 app, socketio = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=False)
